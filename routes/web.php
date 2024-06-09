3d749973319fbae97325bca50e76271cf83ea362
Ngo Tuan Anh <ngotuananh2101@gmail.com>
2024-06-10 00:20:02 +0700

    Remove mongodb because not compatible with activity log

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 include __DIR__ . '/admin.php';
-
-Route::get('/', function () {
-    // Test mongodb connection
-    $name = \Illuminate\Support\Facades\DB::connection('mongodb')->getDatabaseName();
-    echo $name;
-});
